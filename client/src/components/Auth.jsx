a40d1c8601f6f47c44415b533d6bab6239ba6b50
garcia116 <38366630+garcia116@users.noreply.github.com>
2022-02-01 15:45:58 -0800

    removed node_modules

--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -34,7 +34,7 @@ const Auth = () => {
 
        
 
-        const URL = 'http://localhost:5000/auth';
+        const URL = 'https://messaging-app-4.herokuapp.com/auth';
     
 
         const { data: { token, userId, hashedPassword, fullName } } = await axios.post(`${URL}/${isSignup ? 'signup' : 'login'}`, {
